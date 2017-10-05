9040f0ccdfc32f69ff8c2d4ed4f0256afcebdbc1
Dezaimasu <niderlandz@mail.ru>
2017-10-05 18:51:28 +0300

    fixed button behavior when appended to elements with mouse events listeners

--- js/content-script.js
+++ js/content-script.js
@@ -90,6 +90,7 @@ const de_button = {
 
         that.elem = document.createElement('de_cbutton');
         btnElem = that.elem;
+        btnElem.addEventListener('click', that.disableDefaultClick);
         btnElem.addEventListener('contextmenu', that.disableDefaultClick);
         btnElem.addEventListener('mousedown', mousedownListener);
         btnElem.addEventListener('mouseout', mouseoutListener);
